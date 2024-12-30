a369553ee2011c0351ef90f23e9bdcdee519bfc6
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-30 14:02:45 +0700

    1

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app
 from app.models import Event
+import logging
 
 @app.route('/')
 def home():
+    logging.info("Accessing home route")
     events = Event.query.all()
+    logging.info("Fetched %d events from database", len(events))
     return render_template('index.html', events=events)
+
+@app.route('/events')
+def get_events():
+    logging.info("Accessing events route")
+    events = Event.query.all()
+    events_data = [
+        {
+            "date": event.date.strftime('%Y-%m-%d'),
+            "description": event.description
+        } for event in events
+    ]
+    logging.info("Returning %d events", len(events_data))
+    return jsonify(events_data)
