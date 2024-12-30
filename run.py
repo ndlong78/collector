33c2cde1684705f8cf53abf7d85f8a8f2dfee66d
ndlong78 <33077621+ndlong78@users.noreply.github.com>
2024-12-30 14:00:54 +0700

    1

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from app import app, db
 from app.collector import fetch_data, save_data
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         url = 'https://www.thoigian.com.vn/'
+        logging.info("Fetching data from URL: %s", url)
         data = fetch_data(url)
-        save_data(data)  # Đảm bảo save_data được gọi trong ngữ cảnh ứng dụng
-    app.run(debug=True, port=8088)
+        logging.info("Saving data to database")
+        save_data(data)
+    logging.info("Starting Flask app on port 8088")
+    app.run(debug=True, host='0.0.0.0', port=8088)  # Lắng nghe trên tất cả các địa chỉ IP
